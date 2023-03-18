8c80709f87ca8d1dbd162e8b1b9489a01174bade
ahsanrahman0745 <82327593+ahsanrahman0745@users.noreply.github.com>
2023-03-18 12:29:15 +0500

    dev

---
+++ lib/connection/connect.dart
@@ -0,0 +1,42 @@
+import 'package:mysql1/mysql1.dart';
+
+class Connect {
+
+  Connect._providerConstructor();
+  static final Connect instance =  Connect._providerConstructor();
+
+  Future<MySqlConnection> get connection async {
+    var settings = ConnectionSettings(
+        host: '192.168.1.6',
+        port: 3306,
+        user: 'User@1',
+        password: 'User@1',
+        db: 'new_darjee'
+    );
+    return await MySqlConnection.connect(settings);
+  }
+
+
+
+  static fetchCustomerDetails(String query) async {
+    var connection = await instance.connection;
+    return await connection.query(query);
+  }
+
+  static fetchCustomerDetails2(String query) async {
+    var connection = await instance.connection;
+    return await connection.query(query);
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
+
